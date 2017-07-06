157dfc27772dd4d7d47052398788a7b26fdb2383
Rohit Singhal <rsinghal@inautix.co.in>
2017-07-06 22:17:46 +0530

    Added inspiration section

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -9,6 +9,23 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
+
+    require "./class.phpmailer.php"; //include phpmailer class
+      
+    // Instantiate Class  
+    $mail = new PHPMailer();  
+      
+    // Set up SMTP  
+    $mail->IsSMTP();                // Sets up a SMTP connection  
+    $mail->SMTPAuth = true;         // Connection with the SMTP does require authorization    
+    $mail->SMTPSecure = "ssl";      // Connect using a TLS connection  
+    $mail->Host = "smtp.gmail.com";  //Gmail SMTP server address
+    $mail->Port = 465;  //Gmail SMTP port
+    $mail->Encoding = '7bit';
+    
+    // Authentication  
+    $mail->Username   = "atif93011@gmail.com"; // Your full Gmail address
+    $mail->Password   = "Friday@123."; // Your Gmail password
 	
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
@@ -16,10 +33,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'rohit93011@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: atif93011@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
